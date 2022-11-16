8ea47587be0b5b19f4a52dc2b155915b36007204
monir-tuhin <mtuhinsoft@gmail.com>
2022-11-16 17:20:11 +0600

    angular animation first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('userTrigger', [
+      state('false', style({opacity:1})),
+      state('true', style({opacity:0})),
+      transition('* <=> *',[
+        animate(500)
+      ])
+      // transition('visible', [
+      //   style({backgroundColor: 'yellow', opacity: 0}),
+      //   animate(2000)
+      // ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'angular-animation-rnd';
+  title = 'Angular Animation';
+  state = false;
+  animateTest: string | undefined;
+
+  doAnimate() {
+    this.animateTest = 'Animate Me';
+    this.state = !this.state;
+  }
+
+
 }
